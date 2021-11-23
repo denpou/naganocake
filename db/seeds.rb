5000cf584205408ac372e60eef537b3bd22b5300
EC2 Default User <ec2-user@ip-172-31-12-88.ec2.internal>
2021-11-23 23:08:18 +0000

    管理者追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: "aa@a",
-             password:  "000000",
-             password_confirmation: "000000",
-             admin: true)
+User.create!(
+    email: "aa@a",
+    password:  "000000")
