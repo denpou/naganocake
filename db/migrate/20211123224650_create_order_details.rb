a86d09968bd5f15d0af3afa39584a00f17d49959
EC2 Default User <ec2-user@ip-172-31-12-88.ec2.internal>
2021-11-23 23:05:10 +0000

    マイグレート追加

--- db/migrate/20211123224650_create_order_details.rb
+++ db/migrate/20211123224650_create_order_details.rb
@@ -1,7 +1,14 @@
 class CreateOrderDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :order_details do |t|
-
+      t.integer :customer_id, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.integer :shipping_cost, null: false
+      t.integer :total_payment, null: false
+      t.integer :payment_method, default: 0, null: false
+      t.integer :status, default: 0, null: false
       t.timestamps
     end
   end
